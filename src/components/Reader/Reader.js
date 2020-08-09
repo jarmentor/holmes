8ec9dc2997d008dc82cb5414d6cdc1199abc2e8f
Jonathan Armentor <jarmentor@me.com>
2020-08-08 22:34:21 -0500

    Add data parsing

--- src/components/Reader/Reader.js
+++ src/components/Reader/Reader.js
@@ -2,27 +2,31 @@ import React from 'react'
 
 import { CSVReader } from 'react-papaparse'
 
-function parsePlaceholder(data) {
-    console.log('---------------------------')
-    console.log(data)
-    console.log('---------------------------')
-}
+function Reader({ config, updateState, children }) {
+    function defaultParse(data) {
+        console.log('---------------------------')
+        console.log(data)
+        console.log('---------------------------')
+    }
 
-const handlers = {
-    onDrop: (data) => {
-        parsePlaceholder(data)
-    },
-    onError: (err, file, inputElem, reason) => console.error(err),
-    removeFile: (data) => {
-        parsePlaceholder(data)
-    },
-}
+    const handlers = {
+        onDrop: (data) => {
+            defaultParse(data)
+            updateState(data)
+        },
+        onError: (err, file, inputElem, reason) => console.error(err),
+        removeFile: (data) => {
+            defaultParse(data)
+            updateState(null)
+        },
+    }
 
-const Reader = ({ config, children }) => (
-    <CSVReader config={config} {...handlers} addRemoveButton>
-        <span>Drop CSV file here or click to upload.</span>
-    </CSVReader>
-)
+    return (
+        <CSVReader config={config} {...handlers} addRemoveButton>
+            <span>Drop CSV file here or click to upload.</span>
+        </CSVReader>
+    )
+}
 
 // const sortedObj = (state) =>
 //     state
--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Reader } from '../../components'
 
@@ -8,15 +8,72 @@ const Content = styled.main`
     margin: var(--container-padding-small) auto;
     background-color: var(--brand-white);
 `
-const Main = () => (
-    <Content>
-        <Reader
-            config={{
-                header: true,
-                transformHeader: (header) => header.replace(/\s+/g, ''),
-            }}
-        />
-    </Content>
+
+const StyledTable = styled.table`
+    width: 100%;
+    text-align: left;
+
+    td {
+        border: 1px solid currentColor;
+        padding: 1rem 0.25rem;
+    }
+`
+
+const TableHeadings = ({ headings }) => (
+    <thead>
+        <tr>
+            {headings.map((head, key) => (
+                <th key={key}>{head}</th>
+            ))}
+        </tr>
+    </thead>
 )
+
+const TableBody = ({ data }) => (
+    <tbody>
+        {' '}
+        {data.map(({ data: rowData }) => {
+            const {
+                ItemNumber,
+                ItemName,
+                ItemType,
+                ItemDescription,
+                DepartmentName,
+                BriefDescription,
+            } = rowData
+
+            return (
+                <tr>
+                    <td>{ItemNumber}</td>
+                    <td>{ItemName}</td>
+                    <td>{ItemType}</td>
+                    <td>{ItemDescription}</td>
+                    <td>{DepartmentName}</td>
+                    <td>{BriefDescription}</td>
+                </tr>
+            )
+        })}
+    </tbody>
+)
+
+const Main = () => {
+    const [data, setData] = useState(null)
+    const readerConfig = {
+        header: true,
+        transformHeader: (header) => header.replace(/\s+/g, ''),
+        dynamicTyping: true,
+    }
+    return (
+        <Content>
+            <Reader config={readerConfig} updateState={setData} />
+            {data && (
+                <StyledTable>
+                    <TableHeadings headings={data[0].meta.fields} />
+                    <TableBody data={data} />
+                </StyledTable>
+            )}
+        </Content>
+    )
+}
 
 export default Main
